f430009cb8c805529e49a5139b0df3138ad8ae4a
Luis Felipe Botta <luisfsbotta@gmail.com>
2026-07-20 20:18:39 -0300

    Excecões verificadas

---
+++ execoes/src/excecao/verificada/LeitorDocumentos.java
@@ -0,0 +1,30 @@
+package excecao.verificada;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LeitorDocumentos {
+
+    public static void main(String[] args) {
+
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(
+                    new FileReader("C:\\Users\\felip\\OneDrive\\Área de Trabalho\\javanauta\\Docs\\Texto.txt"));
+            //Lendo linha por linha
+            String linha;
+            while ((linha = bufferedReader.readLine()) != null) {
+                System.out.println(linha);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+}
