3d23a7763ea539df7fe571e59c01acf76342e30c
Luis Felipe Botta <luisfsbotta@gmail.com>
2026-07-29 21:24:20 -0300

    JPA

--- projetos/aprendendo-spring/src/main/java/com/javanauta/aprendendospring/AprendendoSpringApplication.java
+++ projetos/aprendendo-spring/src/main/java/com/javanauta/aprendendospring/AprendendoSpringApplication.java
@@ -10,11 +10,7 @@ public class AprendendoSpringApplication {
 	public static void main(String[] args) {
 		//SpringApplication.run(AprendendoSpringApplication.class, args);
 
-		Usuario joao = new Usuario("João","joaodasilva@hotmail.com","senha123");
 
-		System.out.println(joao.getNome());
-		joao.setNome("João da Silva");
-		System.out.println(joao.getNome());
 
 
 	}
